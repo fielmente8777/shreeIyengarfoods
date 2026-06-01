c86a246e2522394161e26ae48f06b51ab36da42b
Personal <sk7627577@gmail.com>
2026-06-01 16:33:24 +0530

    chore: update dependencies and improve TypeScript configuration
    - Updated Next.js from 16.1.1 to 16.2.6
    - Updated React and React DOM from 19.2.0 and 19.0.0 to 19.2.6
    - Updated eslint-config-next from 15.3.1 to 16.2.6
    - Modified tsconfig.json to use "react-jsx" for JSX and added additional include paths for TypeScript

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,30 @@ const nextConfig: NextConfig = {
         hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
       },
     ],
-    // formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp"],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+
+    minimumCacheTTL: 2678400,
+
+    contentDispositionType: "attachment",
     dangerouslyAllowSVG: true,
   },
   trailingSlash: true,
+  compress: true,
+
+  poweredByHeader: false,
+
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["lucide-react", "react-icons", "swiper"],
+
+    optimizeCss: true,
+
+    scrollRestoration: true,
+  },
 };
 
 export default nextConfig;
